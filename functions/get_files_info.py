a47a8ffe3e65dd2ed8358e64fa86f5245576b4c3
JoshuaSE-git <joshua.s.emralino@gmail.com>
2025-06-08 18:40:36 -0700

    file i/o functions

---
+++ functions/get_files_info.py
@@ -0,0 +1,23 @@
+import os
+
+def get_files_info(working_directory, directory=None):
+    abs_wd = os.path.abspath(working_directory)
+    target_dir = abs_wd
+    if directory:
+        path = os.path.join(working_directory, directory)
+        target_dir = os.path.abspath(path)
+    if not target_dir.startswith(abs_wd):
+        return f'Error: Cannot list "{directory}" as it is outside the permitted working directory.'
+    if not os.path.isdir(target_dir):
+        return f'"{directory} is not a directory.'
+
+    try:
+        files_info = []
+        for content in os.listdir(target_dir):
+            path = os.path.join(target_dir, content)
+            file_size = os.path.getsize(path)
+            is_dir = os.path.isdir(path)
+            files_info.append(f'- {content}: file_size={file_size} bytes, is_dir={is_dir}')
+        return "\n".join(files_info)
+    except Exception as e:
+        return f'Error listing files: {e}'
